53c4a57195a7653be3ff98451312791665f08b01
AdrienT34490 <toulouseadrienjob@gmail.com>
2026-03-12 16:17:57 +0100

    update imports

--- MobGenV2/launcher.py
+++ MobGenV2/launcher.py
@@ -1,8 +1,8 @@
 import sys
-
-from . import FLOOR, MOB_TYPE, STATS, SCRIPT_DIR
-from .class_mob import Mob
-from .utils import flat2dice, flat2complexite
+sys.path.append(r"C:\Users\AT280565\PycharmProjects\Perso\RepoMob\MobGenV2")
+from RepoMob.MobGenV2 import FLOOR, MOB_TYPE, STATS, SCRIPT_DIR
+from RepoMob.MobGenV2.class_mob import Mob
+from RepoMob.MobGenV2.utils import flat2dice, flat2complexite
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QPushButton,
     QGridLayout, QLabel, QLineEdit,
